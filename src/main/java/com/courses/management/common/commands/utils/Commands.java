91a15e19c6bc723357699bdfe5d3b4f87eb3cbca
alisabereza <alisabereza@gmail.com>
2020-03-19 14:05:20 +0200

    Find User Command added

--- src/main/java/com/courses/management/common/commands/utils/Commands.java
+++ src/main/java/com/courses/management/common/commands/utils/Commands.java
@@ -1,4 +1,4 @@
-package com.courses.management.common.commands;
+package com.courses.management.common.commands.utils;
 
 public class Commands {
     public static final String HELP = "help";
@@ -15,6 +15,7 @@ public class Commands {
     public static final String CREATE_USER_COURSE = "create_user_course|first_name|last_name|email|course";
     public static final String UPDATE_USER = "update_user";
     public static final String UPDATE_USER_FIRST_NAME = "update_user_first_name";
+    public static final String FIND_USER = "find_user|email";
 
     private Commands() {
         throw new UnsupportedOperationException("Impossible to instantiate util class");
